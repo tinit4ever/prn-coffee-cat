2fec2ee70ab46f1dc96dd5b772ef9dd6fb38a0ea
tinit4ever <tinit4ever@gmail.com>
2024-03-13 19:07:07 +0700

    Done: Account manager feature

--- CoffeeCat/CoffeeCatRazporPage/Pages/Home/Admin.cshtml.cs
+++ CoffeeCat/CoffeeCatRazporPage/Pages/Home/Admin.cshtml.cs
@@ -1,12 +1,28 @@
+using Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repositories.Admin;
 
-namespace CoffeeCatRazporPage.Pages.Home
-{
-    public class AdminModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace CoffeeCatRazporPage.Pages.Home {
+    public class AdminModel : PageModel {
+        private readonly IAdminRepository _adminRepository;
+
+        public AdminModel(IAdminRepository adminRepository) {
+            _adminRepository = adminRepository;
+        }
+        public IEnumerable<User> Users { get; set; }
+        public void OnGet() {
+            Users = _adminRepository.GetAllAccount();
+        }
+
+        public async Task<IActionResult> OnPostBanAsync(int customerId) {
+            await _adminRepository.Ban(customerId);
+            return RedirectToPage("/Home/Admin");
+        }
+
+        public async Task<IActionResult> OnPostUnbanAsync(int customerId) {
+            await _adminRepository.Unban(customerId);
+            return RedirectToPage("/Home/Admin");
         }
     }
 }
