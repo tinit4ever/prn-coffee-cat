7060e8126287f41d9b7b9c82e1f730bbe335f7ea
tinit4ever <tinit4ever@gmail.com>
2024-03-13 15:18:09 +0700

    Done: Registration feature

---
+++ CoffeeCat/CoffeeCatRazporPage/Pages/Auth/Register.cshtml.cs
@@ -0,0 +1,55 @@
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repositories.Auth;
+
+namespace CoffeeCatRazporPage.Pages.Auth {
+    [BindProperties]
+    public class RegisterModel : PageModel {
+        private readonly IRegisterRepository _registerRepository;
+
+        public string name { get; set; }
+        public string useremail { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+
+        public RegisterModel(IRegisterRepository registerRepository) {
+            _registerRepository = registerRepository;
+            name = string.Empty;
+            useremail = string.Empty;
+            password = string.Empty;
+            confirmPassword = string.Empty;
+        }
+        public void OnGet() {
+        }
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
+                return Page();
+            }
+
+            if (password != confirmPassword) {
+                ModelState.AddModelError(string.Empty, "Confirm password is not match");
+                return Page();
+            }
+
+            User userToCreate = new User();
+            // Default value
+            userToCreate.CustomerEnabled = true;
+            userToCreate.RoleId = 3;
+
+            // Dynamic value
+            userToCreate.CustomerName = name;
+            userToCreate.CustomerEmail = useremail;
+            userToCreate.CustomerPassword = password;
+
+            try {
+                await _registerRepository.RegisterAsync(userToCreate);
+                Console.WriteLine("User created successfully.");
+                return RedirectToPage("/Home/Customer");
+            } catch (Exception ex) {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ CoffeeCat/CoffeeCatRazporPage/Pages/Home/Customer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeeCatRazporPage.Pages.Home
+{
+    public class CustomerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
