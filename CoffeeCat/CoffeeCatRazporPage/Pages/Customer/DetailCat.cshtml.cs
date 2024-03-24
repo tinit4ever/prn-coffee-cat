cca57ae357566bafdc8bd49250ea2fa3fcae9857
Phạm Hữu Trí <tripro3214@gmail.com>
2024-03-24 23:30:15 +0700

    add button

--- CoffeeCat/CoffeeCatRazporPage/Pages/Customer/DetailCat.cshtml.cs
+++ CoffeeCat/CoffeeCatRazporPage/Pages/Customer/DetailCat.cshtml.cs
@@ -9,19 +9,24 @@ namespace CoffeeCatRazporPage.Pages.Customer
     public class DetailCatModel : PageModel
     {
         private readonly ICustomerRepository<Cat> repository;
-        public DetailCatModel(ICustomerRepository<Cat> repository)
+        private readonly ICustomerRepository<Area> areaRepository;
+        public DetailCatModel(ICustomerRepository<Cat> repository, ICustomerRepository<Area> areaRepository)
         {
             this.repository = repository;
+            this.areaRepository = areaRepository;
         }
         [BindProperty(SupportsGet = true)]
         public IEnumerable<Cat> Cats { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
         public int AreaId { get; set; }
+        public int? ShopId { get; set; }
         public async Task OnGetAsync(int? pageIndex, int AreaId)
         {
             this.AreaId = AreaId;
             IQueryable<Cat> catsQuery = await repository.GetCatEnableAsync();
+            var area = await areaRepository.GetShopByIdAsync(AreaId);
+            ShopId = area.ShopId;
             catsQuery = catsQuery.Where(a => a.AreaId == this.AreaId);
 
             int pageSize = 5;
